dbf4e65ed4be699b5f669a2383b77653ae5843f6
Borja Arias <borja.arias@eventbrite.com>
2025-06-15 19:58:51 +0200

    Bootstrapped correctly

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -2,6 +2,9 @@
 import * as cdk from 'aws-cdk-lib';
 import { CdkStack } from '../lib/cdk-stack';
 
+const ACCOUNT = '060795922565'
+const REGION = 'eu-west-1';
+
 const app = new cdk.App();
 new CdkStack(app, 'CdkStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
@@ -10,7 +13,10 @@ new CdkStack(app, 'CdkStack', {
 
   /* Uncomment the next line to specialize this stack for the AWS Account
    * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+  env: { 
+    account: ACCOUNT, 
+    region: REGION
+  },
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
